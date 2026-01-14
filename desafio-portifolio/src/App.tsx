d9c4c96e43fb9819e74ce2bd1394f5b074dc0c16
ian.godinho <maxian.godinho@gmail.com>
2026-01-14 11:40:29 -0300

    Add Principal component

--- desafio-portifolio/src/App.tsx
+++ desafio-portifolio/src/App.tsx
@@ -1,10 +1,13 @@
 import { Header } from "./components/Header";
+import { Principal } from "./components/Principal";
 
 function App() {
 
   return (
       <div className="min-h-screen bg-slate-950 text-white">
         <Header />
+
+        <Principal />
       </div>
   );
 }
